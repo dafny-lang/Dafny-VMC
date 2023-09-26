af0d08682e0f411d1b4472d1300c191faf58e9a6
Fabian Zaiser <fzaiser@amazon.com>
2023-09-26 16:39:38 -0400

    Fix Java Uniform

--- interop/java/DRandomUniform.java
+++ interop/java/DRandomUniform.java
@@ -7,8 +7,8 @@ public class DRandomUniform {
   private static SecureRandom r = new SecureRandom();
 
   public static BigInteger Uniform(BigInteger n) {
-    // `longValueExact` will throw an exception if `n` does not fit in a `long`.
-    // see https://docs.oracle.com/javase/8/docs/api/java/math/BigInteger.html#longValueExact--
-    return BigInteger.valueOf(r.nextLong(n.longValueExact()));
+    // `intValueExact` will throw an `ArithmeticException` if `n` does not fit in an `int`.
+    // see https://docs.oracle.com/javase/8/docs/api/java/math/BigInteger.html#intValueExact--
+    return BigInteger.valueOf(r.nextInt(n.intValueExact()));
   }
 }
