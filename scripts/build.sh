a603a3c641038ae6c51bb2a733e32f97850db4b3
John Tristan <trjohnb@amazon.com>
2023-09-26 17:58:51 -0400

    Builds in CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
 
-DAFNY=dafny
-LANG=java
+set -e
 
-$DAFNY build --target:$LANG interop/$LANG/DRandomCoin.$LANG interop/$LANG/DRandomUniform.$LANG src/Dafny-VMC.dfy -o build/$LANG/Dafny-VMC.jar
+if [ -z ${DAFNY+x} ]
+then
+  echo "Set the DAFNY variable"
+  exit 1
+fi
+
+if [ -z ${LANG+x} ]
+then
+  echo "Set the LANG variable"
+  exit 1
+fi
+
+$DAFNY build --target:$LANG interop/$LANG/DRandomCoin.$LANG interop/$LANG/DRandomUniform.$LANG src/Dafny-VMC.dfy -o build/$LANG/Dafny-VMC
