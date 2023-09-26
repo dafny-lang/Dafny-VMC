538a45c38407d8086934e39b489cecf86d3a38cb
John Tristan <trjohnb@amazon.com>
2023-09-26 16:18:12 -0400

    Version number

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/dafny-lang/dafny/releases/download/v4.2.0/dafny-4.2.0-x64-ubuntu-20.04.zip
-unzip dafny-4.2.0-x64-ubuntu-20.04.zip
+VERSION=https://github.com/dafny-lang/dafny/releases/download/v4.2.0/dafny-4.2.0-x64-ubuntu-20.04.zip
+
+wget $VERSION
+unzip `basename $VERSION` 
