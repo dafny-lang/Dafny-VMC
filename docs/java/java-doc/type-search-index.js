8f12d2157467e623904c6a1a6dda64a3af07ff31
John Tristan <trjohnb@amazon.com>
2023-09-28 10:37:23 -0400

    Improved

---
+++ docs/java/java-doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"DafnyVMC","l":"DRandomExternUniform"},{"p":"DafnyVMC","l":"DRandomFoundational"}];updateSearchResults();
