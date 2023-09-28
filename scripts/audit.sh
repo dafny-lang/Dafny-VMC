5a0e25d0f0078e7642e6334660427090b0829359
John Tristan <trjohnb@amazon.com>
2023-09-28 11:30:19 -0400

    audit file

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -8,10 +8,10 @@ then
   exit 1
 fi
 
+echo '\n' > audit.log
+
 for file in `find ./src -type f -name '*.dfy'`
 do
-  echo Auditing $file 
-  #cat $file | grep "axiom"
-  #cat $file | grep "assume"
-  $DAFNY audit $file | grep -v '{:termination false}\|{:extern}\|decreases *\|Dafny auditor completed\|Dafny program verifier' | sed 's/.*Warning://' | sed 's/Possible.*//'
-done 
+  echo Auditing $file >> audit.log
+  $DAFNY audit $file | grep -v '{:termination false}\|{:extern}\|decreases *\|Dafny auditor completed\|Dafny program verifier' | sed 's/.*Warning://' | sed 's/Possible.*//' >> audit.log
+done
