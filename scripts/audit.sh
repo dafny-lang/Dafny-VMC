54f849f8802d532f2132045cdbcd42dda884f3d4
John Tristan <trjohnb@amazon.com>
2023-09-28 13:53:40 -0400

    workflow audit

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -8,9 +8,9 @@ then
   exit 1
 fi
 
-echo '\n' > audit.log
+echo '' > audit.log
 
-for file in `find ./src -type f -name '*.dfy'`
+for file in `find ./src -type f -name '*.dfy' | xargs -n1 | sort | xargs`
 do
   echo Auditing $file >> audit.log
   $DAFNY audit $file | grep -v '{:termination false}\|{:extern}\|decreases *\|Dafny auditor completed\|Dafny program verifier' | sed 's/.*Warning://' | sed 's/Possible.*//' >> audit.log
