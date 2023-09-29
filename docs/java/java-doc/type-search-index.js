6614985e8d3605e33c5bdae24af1ff7b54d588d0
John Tristan <trjohnb@amazon.com>
2023-09-29 17:17:31 -0400

    removing old javadoc

--- docs/java/java-doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"DafnyVMC","l":"DRandomExternUniform"},{"p":"DafnyVMC","l":"DRandomFoundational"}];updateSearchResults();
