616a4911704972c632bad9c83657794d090c7679
stefan-aws <stefanze@amazon.com>
2023-10-05 15:01:38 +0100

    fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,12 +16,10 @@ fi
 
 echo Running $TARGET_LANG tests...
 echo "Running tests/TestsFoundational.dfy:"
-time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG tests/TestsFoundational.dfy Tests.toml --no-verify
+time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG tests/TestsFoundational.dfy tests/Tests.dfy dfyconfig.toml --no-verify
 echo "Running tests/TestsExternUniform.dfy:"
-time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG tests/TestsExternUniform.dfy Tests.toml --no-verify
+time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG tests/TestsExternUniform.dfy tests/Tests.dfy dfyconfig.toml  --no-verify
 
 echo Running $TARGET_LANG documentation...
-echo "Running docs/dafny/ExamplesFoundational.dfy"
-$DAFNY run DocsFoundational.toml --target:$TARGET_LANG --input src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG --input src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG --no-verify
-echo "Running docs/dafny/ExamplesExternUniform.dfy"
-$DAFNY run DocsExternUniform.toml --target:$TARGET_LANG --input src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG --input src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG --no-verify
+echo "Running docs/dafny/ExamplesFoundational.dfy and docs/dafny/ExamplesExternUniform.dfy" 
+$DAFNY run dfyconfig.toml --target:$TARGET_LANG --input src/interop/$TARGET_LANG/DRandomCoin.$TARGET_LANG --input src/interop/$TARGET_LANG/DRandomUniform.$TARGET_LANG --no-verify
--- scripts/verify.sh
+++ scripts/verify.sh
@@ -10,3 +10,8 @@ fi
 
 echo Verifying the proofs...
 time $DAFNY verify dfyconfig.toml
+time $DAFNY verify docs/dafny/ExamplesExternUniform.dfy
+time $DAFNY verify docs/dafny/ExamplesFoundational.dfy
+time $DAFNY verify tests/Tests.dfy
+time $DAFNY verify tests/TestsExternUniform.dfy
+time $DAFNY verify tests/TestsFoundational.dfy
