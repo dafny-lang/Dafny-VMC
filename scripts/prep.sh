15b319a03053cd5c346f110e46007e16efa18a33
stefan-aws <stefanze@amazon.com>
2023-11-08 14:39:18 +0000

    back

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-VERSION=https://github.com/dafny-lang/dafny/archive/refs/tags/v4.3.0.zip
+VERSION=https://github.com/dafny-lang/dafny/releases/download/v4.3.0/dafny-4.3.0-x64-ubuntu-20.04.zip
 
 wget $VERSION
-unzip `basename $VERSION` 
-
-cd dafny-4.3.0
-make exe
-cd ..
+unzip `basename $VERSION`
