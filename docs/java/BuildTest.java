60fbc694758a34f11ea9c11100c4de81df5df53b
jtristan <trjohnb@amazon.com>
2024-02-13 16:32:28 -0500

    fixed build

--- docs/java/BuildTest.java
+++ docs/java/BuildTest.java
@@ -13,30 +13,28 @@ class Check {
         boolean[] arr5 = {true, false, false, true};
         long[] arr6 = {111111L, 333333L, 999999L};
         short[] arr7 = {-3, 0, 3};
-        Rationals.Rational gamma = new Rationals.Rational(BigInteger.valueOf(3), BigInteger.valueOf(5));
+        BigInteger num = BigInteger.valueOf(3);
+        BigInteger deb = BigInteger.valueOf(5);
         
         /* STANDARD RNG */
         System.out.println("\nSTANDARD RNG TESTS\n");
         
         DafnyVMC.Random r = new DafnyVMC.Random();
 
-        System.out.println("Example of Coin sampling");
-        System.out.println(r.CoinSample());
-
         System.out.println("Example of Uniform sampling");
         System.out.println(r.UniformSample(BigInteger.valueOf(4)));
 
         System.out.println("Example of Bernoulli sampling");
-        System.out.println(r.BernoulliSample(gamma));
+        System.out.println(r.BernoulliSample(num,den));
 
         System.out.println("Example of BernoulliExpNeg sampling");
-        System.out.println(r.BernoulliExpNegSample(gamma));
+        System.out.println(r.BernoulliExpNegSample(num,den));
 
         System.out.println("Example of DiscreteGaussian sampling");
-        System.out.println(r.DiscreteGaussianSample(gamma));
+        System.out.println(r.DiscreteGaussianSample(num,den));
 
         System.out.println("Example of DiscreteLaplace sampling");
-        System.out.println(r.DiscreteLaplaceSample(gamma));
+        System.out.println(r.DiscreteLaplaceSample(num,den));
 
         System.out.println("Example of Fisher-Yates: BigInteger");
         r.Shuffle(arr1);
