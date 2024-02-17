9953684c9da95232bbaadb74f99a0c917d05dc90
stefan-aws <stefanze@amazon.com>
2024-02-17 18:52:09 +0000

    change interop filename, simplify scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,22 +17,12 @@ fi
 echo Running $TARGET_LANG tests...
 
 echo "Running tests/TestsRandom.dfy:"
-if [ "$TARGET_LANG" = "py" ]
-then
-  time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/Full/DafnyVMC.$TARGET_LANG src/interop/$TARGET_LANG/Part/DafnyVMCPartMaterial.$TARGET_LANG tests/TestsRandom.dfy tests/Tests.dfy dfyconfig.toml  --no-verify
-else
-  time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/Full/Random.$TARGET_LANG src/interop/$TARGET_LANG/Part/Random.$TARGET_LANG tests/TestsRandom.dfy tests/Tests.dfy dfyconfig.toml  --no-verify
-fi
+time $DAFNY test --target:$TARGET_LANG src/interop/$TARGET_LANG/Full/Random.$TARGET_LANG src/interop/$TARGET_LANG/Part/Random.$TARGET_LANG tests/TestsRandom.dfy tests/Tests.dfy dfyconfig.toml  --no-verify
 
 echo Running $TARGET_LANG documentation...
 
 echo "Building docs/dafny/ExamplesRandom.dfy..." 
-if [ "$TARGET_LANG" = "py" ]
-then
-  time $DAFNY build docs/dafny/ExamplesRandom.dfy --target:$TARGET_LANG src/interop/$TARGET_LANG/Full/DafnyVMC.$TARGET_LANG src/interop/$TARGET_LANG/Part/DafnyVMCPartMaterial.$TARGET_LANG dfyconfig.toml --no-verify
-else
-  time $DAFNY build docs/dafny/ExamplesRandom.dfy --target:$TARGET_LANG src/interop/$TARGET_LANG/Full/Random.$TARGET_LANG src/interop/$TARGET_LANG/Part/Random.$TARGET_LANG dfyconfig.toml --no-verify
-fi
+time $DAFNY build docs/dafny/ExamplesRandom.dfy --target:$TARGET_LANG src/interop/$TARGET_LANG/Full/Random.$TARGET_LANG src/interop/$TARGET_LANG/Part/Random.$TARGET_LANG dfyconfig.toml --no-verify
 
 echo "Executing compiled docs/dafny/ExamplesRandom.dfy:" 
 if [ "$TARGET_LANG" = "java" ]
