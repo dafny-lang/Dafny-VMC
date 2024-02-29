749a41b1ee167d542d1a90d9a9789b53795cea14
stefan-aws <stefanze@amazon.com>
2024-02-29 19:43:03 +0000

    edge cases and presentation

--- docs/java/TestSamplers.java
+++ docs/java/TestSamplers.java
@@ -19,30 +19,47 @@ class TestSamplers {
                 System.out.println("Testing Uniform(" + a + ")");
                 System.out.println(r.UniformSample(i));
 
-                for (int b = 1; b <= a; b++) {
+                for (int b = 1; b <= 1000; b++) {
                     BigInteger j = BigInteger.valueOf(b);
 
                     System.out.println("Testing Bernoulli(" + a + "/" + b + ")");
                     System.out.println(r.BernoulliSample(i, j));
-                    System.out.println("Testing Bernoulli(" + b + "/" + a + ")");
-                    System.out.println(r.BernoulliSample(j, i));
 
                     System.out.println("Testing BernoulliExpNeg(" + a + "/" + b + ")");
                     System.out.println(r.BernoulliExpNegSample(i, j));
-                    System.out.println("Testing BernoulliExpNeg(" + b + "/" + a + ")");
-                    System.out.println(r.BernoulliExpNegSample(j, i));
 
                     System.out.println("Testing DiscreteGaussian(" + a + "/" + b + ")");
                     System.out.println(r.DiscreteGaussianSample(i, j));
-                    System.out.println("Testing DiscreteGaussian(" + b + "/" + a + ")");
-                    System.out.println(r.DiscreteGaussianSample(j, i));
       
                     System.out.println("Testing DiscreteLaPlace(" + a + "/" + b + ")");
                     System.out.println(r.DiscreteLaplaceSample(i, j));
-                    System.out.println("Testing DiscreteLaPlace(" + b + "/" + a + ")");
-                    System.out.println(r.DiscreteLaplaceSample(j, i));
                 }
             }
+
+            // Edge cases
+
+            BigInteger k = BigInteger.valueOf(1000000);
+            BigInteger l = BigInteger.valueOf(1);
+
+            System.out.println("Testing Bernoulli(1000000, 1)");
+            System.out.println(r.BernoulliSample(k, l));
+            System.out.println("Testing Bernoulli(1, 1000000)");
+            System.out.println(r.BernoulliSample(l, k));
+
+            System.out.println("Testing BernoulliExpNeg(1000000, 1)");
+            System.out.println(r.BernoulliExpNegSample(k, l));
+            System.out.println("Testing BernoulliExpNeg(1, 1000000)");
+            System.out.println(r.BernoulliExpNegSample(l, k));
+
+            System.out.println("Testing DiscreteGaussianSample(1000000, 1)");
+            System.out.println(r.DiscreteGaussianSample(k, l));
+            System.out.println("Testing DiscreteGaussianSample(1, 1000000)");
+            System.out.println(r.DiscreteGaussianSample(l, k));
+
+            System.out.println("Testing DiscreteLaplace(1000000, 1)");
+            System.out.println(r.DiscreteLaplaceSample(k, l));
+            System.out.println("Testing DiscreteLaplace(1, 1000000)");
+            System.out.println(r.DiscreteLaplaceSample(l, k));
 
             /* CUSTOM RNG */
             System.out.println("\nCUSTOM RNG TESTS\n");
@@ -55,29 +72,44 @@ class TestSamplers {
                 System.out.println("Testing Uniform(" + a + ")");
                 System.out.println(t.UniformSample(i));
 
-                for (int b = 1; b <= a; b++) {
+                for (int b = 1; b <= 1000; b++) {
                     BigInteger j = BigInteger.valueOf(b);
                     System.out.println("Testing Bernoulli(" + a + "/" + b + ")");
                     System.out.println(t.BernoulliSample(i, j));
-                    System.out.println("Testing Bernoulli(" + b + "/" + a + ")");
-                    System.out.println(t.BernoulliSample(j, i));
 
                     System.out.println("Testing BernoulliExpNeg(" + a + "/" + b + ")");
                     System.out.println(t.BernoulliExpNegSample(i, j));
-                    System.out.println("Testing BernoulliExpNeg(" + j + "/" + i + ")");
-                    System.out.println(t.BernoulliExpNegSample(j, i));
 
                     System.out.println("Testing DiscreteGaussian(" + a + "/" + b + ")");
                     System.out.println(t.DiscreteGaussianSample(i, j));
-                    System.out.println("Testing DiscreteGaussian(" + b + "/" + a + ")");
-                    System.out.println(t.DiscreteGaussianSample(j, i));
       
                     System.out.println("Testing DiscreteLaPlace(" + a + "/" + b + ")");
                     System.out.println(t.DiscreteLaplaceSample(i, j));
-                    System.out.println("Testing DiscreteLaPlace(" + b + "/" + a + ")");
-                    System.out.println(t.DiscreteLaplaceSample(j, i));
                 }
             }
+
+
+            // Edge cases
+
+            System.out.println("Testing Bernoulli(1000000, 1)");
+            System.out.println(t.BernoulliSample(k, l));
+            System.out.println("Testing Bernoulli(1, 1000000)");
+            System.out.println(t.BernoulliSample(l, k));
+
+            System.out.println("Testing BernoulliExpNeg(1000000, 1)");
+            System.out.println(t.BernoulliExpNegSample(k, l));
+            System.out.println("Testing BernoulliExpNeg(1, 1000000)");
+            System.out.println(t.BernoulliExpNegSample(l, k));
+
+            System.out.println("Testing DiscreteGaussianSample(1000000, 1)");
+            System.out.println(t.DiscreteGaussianSample(k, l));
+            System.out.println("Testing DiscreteGaussianSample(1, 1000000)");
+            System.out.println(t.DiscreteGaussianSample(l, k));
+
+            System.out.println("Testing DiscreteLaplace(1000000, 1)");
+            System.out.println(t.DiscreteLaplaceSample(k, l));
+            System.out.println("Testing DiscreteLaplace(1, 1000000)");
+            System.out.println(t.DiscreteLaplaceSample(l, k));
 
 
     }
