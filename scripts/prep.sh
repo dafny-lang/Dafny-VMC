d6e159bd38af3d5d063baf089da8918a688614b4
jtristan <trjohnb@amazon.com>
2024-04-24 09:58:30 -0400

    changed elan install

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -5,7 +5,4 @@ VERSION=https://github.com/dafny-lang/dafny/releases/download/v4.4.0/dafny-4.4.0
 wget $VERSION
 unzip `basename $VERSION` 
 
-set -o pipefail
-          curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
-          ./elan-init -y --default-toolchain none
-          echo "$HOME/.elan/bin" >> $GITHUB_PATH
+curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf | sh
