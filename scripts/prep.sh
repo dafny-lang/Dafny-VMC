cefe0f6248f3c00214cb5355e1bb5e45a1c3d92c
stefan-aws <stefanze@amazon.com>
2024-04-25 16:47:38 +0100

    small changes

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -4,12 +4,3 @@ VERSION=https://github.com/dafny-lang/dafny/releases/download/v4.4.0/dafny-4.4.0
 
 wget $VERSION
 unzip `basename $VERSION` 
-
-pushd SampCert
-set -o pipefail
-curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
-./elan-init -y --default-toolchain leanprover/lean4:v4.7.0
-$HOME/.elan/bin/lake build VMC
-popd
-
-
