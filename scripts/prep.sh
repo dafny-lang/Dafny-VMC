617d7b045517b5e8fecfae6ab28b2ef497d59051
jtristan <trjohnb@amazon.com>
2024-04-25 09:37:07 -0400

    Trying Lean4 install

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -9,6 +9,7 @@ pushd SampCert
 set -o pipefail
 curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
 ./elan-init -y --default-toolchain leanprover/lean4:v4.7.0
+ls "$HOME/.elan/bin"
 echo "$HOME/.elan/bin" >> $GITHUB_PATH
 lake build VMC
 popd
