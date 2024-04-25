aff71e7d0bd8a9dccc3b5ed61ec23c97114c3340
jtristan <trjohnb@amazon.com>
2024-04-25 09:22:30 -0400

    Trying Lean4 install

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -4,3 +4,9 @@ VERSION=https://github.com/dafny-lang/dafny/releases/download/v4.4.0/dafny-4.4.0
 
 wget $VERSION
 unzip `basename $VERSION` 
+
+set -o pipefail
+curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+./elan-init -y --default-toolchain none
+echo "$HOME/.elan/bin" >> $GITHUB_PATH
+
