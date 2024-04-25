d0aeab39bd599de6e83ba72c1160dba035644088
jtristan <trjohnb@amazon.com>
2024-04-25 09:28:45 -0400

    Trying Lean4 install

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -5,13 +5,12 @@ VERSION=https://github.com/dafny-lang/dafny/releases/download/v4.4.0/dafny-4.4.0
 wget $VERSION
 unzip `basename $VERSION` 
 
-# set -o pipefail
-# curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
-# ./elan-init -y --default-toolchain none
-# echo "$HOME/.elan/bin" >> $GITHUB_PATH
-
-wget -q https://raw.githubusercontent.com/leanprover-community/mathlib4/master/scripts/install_debian.sh && bash install_debian.sh ; rm -f install_debian.sh && source ~/.profile
-
 pushd SampCert
+set -o pipefail
+curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+./elan-init -y --default-toolchain none
+echo "$HOME/.elan/bin" >> $GITHUB_PATH
 lake build VMC
 popd
+
+
